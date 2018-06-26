1fc6a89e40d44d628b489760f0bfd0f49479d55f
Felipe Ramos <felipecramos@ufrn.edu.br>
2018-06-26 17:53:27 -0300

    Codes got prettier

--- include/mempool_common.hpp
+++ include/mempool_common.hpp
@@ -4,53 +4,59 @@
 #include <stdlib.h>
 #include <iostream> 
 #include <new>
+
+#define debug_constructor true
+#define debug_allocate true
+
 using size_type = size_t;
 
 /** The minimal structure of other storage management classes. */ 
 class StoragePool
 {
 	public:
-		virtual ~StoragePool();
+		virtual ~StoragePool() { /* empty */ };
 		/** Reserves size_t bytes and returns as a pointer. */
 		virtual void * Allocate( size_t ) = 0;
 		/** Recieve a pointer to a reserved region and free's it. */
 		virtual void Free( void * ) = 0;
 };
 
-class SLPool/* : public StoragePool*/
+class SLPool : public StoragePool
 {
 	using size_type = size_t;
 	private: 
+		/** Enumeration for status values. */
 		enum status
 		{
 			m_free,
 			m_used
 		};
 		
+		/** Where the info about the block is located. */
 		struct Header{
 			unsigned int m_length;
 			Header() : m_length(0u) {};
+			Header( unsigned int _size ) : m_length(_size) {};
 		};
 
 
+		/** Tells which SLPool this block is from. */
 		struct Tag
 		{
 			SLPool *pool;
 		};
 
+		/** Main struct for the data. */
 		struct Block : public Header
 		{
-
 			enum { BlockSize = 16 };
-		
 			union{
-
 				Block *m_next;
 				char m_raw[ BlockSize - sizeof(Header) ];
 			};
 			
-			Block( ): Header() ,m_next(nullptr) {}
-
+			/* Default constructor */
+			Block(): Header() ,m_next(nullptr) {}
 		};
 
 		unsigned int m_n_blocks;
@@ -59,18 +65,12 @@ class SLPool/* : public StoragePool*/
 
 
 	public:
-		explicit SLPool(size_type bytes);
-
+		explicit SLPool( size_type );
 		~SLPool();
 
-		void * Allocate (size_type );
+		void * Allocate ( size_type );
 	//	void Release (Tag *);
 		void Free (void *);
-		
-	//	void * operator new( size_type bytes, SLPool & p );
-		
-	//	void* operator new(size_t bytes);
-	//	void operator delete(void*arg)noexcept;
 };
 
 #endif
--- src/mempool_common.cpp
+++ src/mempool_common.cpp
@@ -1,125 +1,129 @@
 #include "mempool_common.hpp"
 
+SLPool::SLPool( size_type bytes ){
+	this->m_pool = (Block *) malloc( bytes + sizeof(Block) );
+	this->m_pool->m_length = bytes;
+	this->m_pool->m_next = nullptr;
 
-SLPool::SLPool(size_type bytes){
-	m_pool = (Block * ) malloc(bytes+sizeof(Block));
-	m_pool->m_length = bytes;
-	m_pool->m_next = nullptr;
-
-	m_sentinel = m_pool;
-	m_sentinel += bytes/sizeof(Block);
-	m_sentinel->m_length = 0;
-	m_sentinel->m_next = m_pool;
+	this->m_sentinel = this->m_pool;
+	this->m_sentinel += bytes / sizeof(Block);
+	this->m_sentinel->m_length = 0;
+	this->m_sentinel->m_next = this->m_pool;
 
 
-	std::cout << "Size of block char: " << sizeof(char[Block::BlockSize]) << std::endl;
-	std::cout << "Size of block *: " << sizeof(Block *) << std::endl;
-	std::cout << "Size of block char minus header: " << sizeof(char[Block::BlockSize-sizeof(Header)]) << std::endl;
-	std::cout << "Size of block: " << sizeof(Block) << std::endl;
-	std::cout << "Size of header: "<< sizeof(Header) << std::endl;
-	std::cout << m_pool->m_length << std::endl;
-	std::cout << m_sentinel->m_next->m_length << std::endl;
+	if( debug_constructor ){
+		std::cout << "=== DEBUG ==="
+			<< "\nSize of block char: " 
+			<< sizeof( char[Block::BlockSize] )
+			<< "\nSize of block *: " << sizeof(Block *)
+			<< "\nSize of block char minus header: " 
+			<< sizeof( char[Block::BlockSize-sizeof(Header)] )
+			<< "\nSize of block: " << sizeof(Block)
+			<< "\nSize of header: "<< sizeof(Header)
+			<< "\n" << this->m_pool->m_length
+			<< "\n" << this->m_sentinel->m_next->m_length << std::endl;
+	}
 }
-
 
 SLPool::~SLPool(){
-
-	free(m_pool);
-
+	free(this->m_pool);
 }
 
-void * SLPool::Allocate(size_type bytes){
-	
-	auto ptr = m_sentinel->m_next;
-	Block * before_begin = ptr;
-	auto bytes_to_alloc =  (bytes+sizeof(Header))/sizeof(Block);
+void * SLPool::Allocate( size_type bytes ){
+	Block * m_ptr = this->m_sentinel->m_next;
 
-	while(ptr != nullptr){
-		if(ptr->m_length >= bytes_to_alloc )
+	Block * before_begin = m_ptr;
+	auto bytes_to_alloc = ( bytes + sizeof(Header) ) / sizeof(Block);
+
+	while( m_ptr != nullptr )
+	{
+		if( m_ptr->m_length >= bytes_to_alloc )
 		{
-			if(ptr->m_length == bytes_to_alloc){
-				before_begin->m_next = ptr->m_next;
-				return reinterpret_cast<Header *> (ptr + 1U);
+			if( m_ptr->m_length == bytes_to_alloc )
+			{
+				before_begin->m_next = m_ptr->m_next;
+				return reinterpret_cast<Header *> (m_ptr) + (1U);
 			}
-			
-			else{
-				before_begin->m_next = (Block *) (ptr+ptr->m_length);
-				return reinterpret_cast<Header *> (ptr + 1U);
+			else
+			{
+				before_begin->m_next = (Block *) ( m_ptr + m_ptr->m_length );
+				return reinterpret_cast<Header *> (m_ptr) + (1U);
 			}
 
-			before_begin = ptr;
-			ptr = ptr->m_next;
+			before_begin = m_ptr;
+			m_ptr = m_ptr->m_next;
 		}
 	}
 
 	throw std::bad_alloc();
 }
 
-void SLPool::Free(void * ptr){
+void SLPool::Free( void * ptr ){
 
-	ptr = reinterpret_cast<Block *> (reinterpret_cast < Header *> (ptr)) - 1U;
+	ptr = reinterpret_cast<Block *> (reinterpret_cast <Header *> (ptr)) - 1U;
 	
-	auto ptr_aux = m_sentinel->m_next;
+	Block * ptr_aux = m_sentinel->m_next;
 	Block * input_ptr = (Block *) ptr;
 	Block * before_begin = ptr_aux;
 	Block * before_prev = nullptr;
 	Block * ptr_prev = nullptr;
 	Block * ptr_post = nullptr;
 
-	while( ptr_aux != nullptr)
+	while( ptr_aux != nullptr )
 	{
-		if(ptr_aux < ptr){
-			if(ptr_aux + ptr_aux->m_length == ptr){
+		if(( ptr_aux < ptr ) and ( ptr_aux + ptr_aux->m_length == ptr ))
+		{
 			before_prev = before_begin;
 			ptr_prev = ptr_aux;
-				}
 		}
-		else if(ptr_aux - ptr_aux->m_length == ptr){
-
-		ptr_post = ptr_aux;
-		before_begin->m_next = before_begin->m_next->m_next;
-		input_ptr->m_length += ptr_aux->m_length;
-		ptr_aux->m_length = 0;
+		else if( ptr_aux - ptr_aux->m_length == ptr )
+		{
+			ptr_post = ptr_aux;
+			before_begin->m_next = before_begin->m_next->m_next;
+			input_ptr->m_length += ptr_aux->m_length;
+			ptr_aux->m_length = 0;
 		}
 		
 		before_begin = ptr_aux;
 		ptr_aux = ptr_aux->m_next;
 		
-		if(ptr_prev != nullptr and ptr_post != nullptr){
+		if( ptr_prev != nullptr and ptr_post != nullptr )
+		{
 			ptr_prev->m_length += input_ptr->m_length;
 			break;
 		}
-
 	}
 	
-	if(ptr_aux == nullptr and ptr_prev != nullptr){
+	if( ptr_aux == nullptr and ptr_prev != nullptr )
 		ptr_prev->m_length += input_ptr->m_length;
-	}
 }
 
-
-
-		/*void * SLPool::operator new( size_type bytes, SLPool & p ){
-			Tag* const tag = reinterpret_cast<Tag *> (p.Allocate( bytes * sizeof(Tag)) ); 
-			tag->pool = &p;
-
-			return (reinterpret_cast<void *> (tag + 1U) );
-		}
-		void* SLPool::operator new(size_t bytes){//Regula rnew
-			Tag* const tag = reinterpret_cast <Tag *> (malloc(bytes+sizeof(Tag) ) );
-			tag->pool=nullptr;
-			
-			return(reinterpret_cast<void*>(tag+1U) );
-		}*/
 /*
-		void SLPool::operator delete(void*arg)noexcept{
-			//We need to subtract 1U(infact,pointer arithmetics)because arg
-			//points to the raw data(second block of information).
-			//The pool id(tag) is located ‘sizeof(Tag)’bytes before.
-			
-			Tag* const tag = reinterpret_cast<Tag*> (arg)-1U;
-			if(nullptr!=tag->pool)//Memory block belongs to a particularGM.
-				tag->pool->Release(tag);
-			else
-				free(tag);//Memory block belongs to the operational system.
-		}*/
+
+void * SLPool::operator new( size_type bytes, SLPool & p ){
+	Tag* const tag =
+		reinterpret_cast<Tag *> (p.Allocate( bytes * sizeof(Tag)) ); 
+	tag->pool = &p;
+
+	return (reinterpret_cast<void *> (tag + 1U) );
+}
+void* SLPool::operator new(size_t bytes){//Regula rnew
+	Tag* const tag =
+		reinterpret_cast <Tag *> (malloc(bytes+sizeof(Tag)));
+	tag->pool=nullptr;
+	
+	return(reinterpret_cast<void*>(tag+1U) );
+}
+void SLPool::operator delete(void*arg)noexcept{
+	//We need to subtract 1U(infact,pointer arithmetics)because arg
+	//points to the raw data(second block of information).
+	//The pool id(tag) is located ‘sizeof(Tag)’bytes before.
+	
+	Tag* const tag = reinterpret_cast<Tag*> (arg)-1U;
+	if(nullptr!=tag->pool)//Memory block belongs to a particularGM.
+		tag->pool->Release(tag);
+	else
+		free(tag);//Memory block belongs to the operational system.
+}
+
+*/
